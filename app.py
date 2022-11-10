a88d3db030b1174f112cabe73901657f7d02de01
kingslayer929 <percylin0929@gmail.com>
2022-11-10 23:22:01 +0800

    fix src & href in index and add some path

--- app.py
+++ app.py
@@ -4,18 +4,33 @@ import dbconnect
 app = Flask(__name__) # __name__ 代表目前執行的模組
 
 @app.route("/") # 函式的裝置 (Decorator): 以函式為基礎，提供附加的功能
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/KPI_1")
 def showDB():
     data = dbconnect.select_all("kpi_1")
     return render_template("KPI_1.html", data = data)
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/checkAuth", methods=['POST'])
+def checkAuth():
+    if request.method == "POST":
+        # === check username & password ===
+        if 1:
+            return redirect(url_for("admin/backend"))
+        # =================================
+    return redirect(url_for("login", fail = True))
+
+@app.route("/admin/backend")
+def backend_page():
+    # === check auth ===
     
+    # ==================
+    return render_template("admin/backend_html")
 
 if __name__ == "__main__": # 如果以上程式執行
     app.run(debug=True) # 立刻啟動伺服器
