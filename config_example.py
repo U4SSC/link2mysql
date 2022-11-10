0d53c5dae70871165ccbf23a34e4b634242bab6e
Davinci <ericchen1201@alum.ccu.edu.tw>
2022-11-10 14:06:52 +0800

    DB CONNECT WITH PADNAS VERSION

--- config_example.py
+++
@@ -1,5 +0,0 @@
-DB_HOST = "localhost" # 要連線到的主機
-DB_PORT = 3306 # 連線的port
-DB_NAME = "soft_engr" # 資料庫名稱
-DB_USER = "root" # 資料庫使用者帳號
-DB_PASSWD = "" # 資料庫使用者密碼
--- dbconnect.py
+++ dbconnect.py
@@ -1,11 +1,13 @@
 import config
+import pandas as pd
 import pymysql
 import pymysql.cursors
 
 conn = pymysql.connect(host=config.DB_HOST, port=config.DB_PORT, user=config.DB_USER, password=config.DB_PASSWD, db=config.DB_NAME)
 
 def select_all(tablename:str):
-    with conn.cursor() as cursor:
-        sql = "SELECT * FROM `"+tablename+"`"
-        cursor.execute(sql)
-        return cursor.fetchall()
+    return pd.read_sql("SELECT * FROM `"+tablename+"`", conn)
+#     with conn.cursor() as cursor:
+#         sql = "SELECT * FROM `"+tablename+"`"
+#         cursor.execute(sql)
+#         return cursor.fetchall()
